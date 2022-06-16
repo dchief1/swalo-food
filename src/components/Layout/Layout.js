6740b7d85aaa108a532045027d282a74f06f2b3f
dchief1 <igwesiemmanuel4@gmail.com>
2022-06-16 16:18:06 +0100

    finished the text effect

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Content from '../Content/Content'
 import Header from '../Header/Header'
 
 
 const Layout = () => {
   return (
-    <div className='bg-orange-50'>
+    <div className=''>
         <Header />
+        <Content />
     </div>
   )
 }
