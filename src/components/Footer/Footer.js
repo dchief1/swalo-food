bb53ec7312c20f8cc6afeab594eac03943eb7f95
dchief1 <igwesiemmanuel4@gmail.com>
2022-06-17 12:46:06 +0100

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -150,7 +150,7 @@ const Footer = () => {
                         </ul>   
                         </div> 
                     </div>
-                </div>
+               
 
             </div>
         </>
